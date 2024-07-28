bf1afa31e4d84908d0a0b9fd9c9f075a095a4909
drixitel <drixitel@gmail.com>
2024-07-27 22:32:31 -0700

    added teaching and leadership entry

---
+++ content/resume-entries/teaching_leadership/index.ts
@@ -0,0 +1,23 @@
+import { ResumeSection } from "..";
+
+
+export const teaching_leadership: ResumeSection = {
+  title: "Teaching & Leadership",
+  entries: [
+    {
+      title: "Freelance STEM Tutor",
+      date: "Feb 2017 - Present",
+      location: "Mathematics (Trig - Calc), Physics (Engineering, E&M, Mech, Thermo, Quantum), Scientific Computation",
+      description: "",
+      tags: [],
+    },
+    {
+      title: "Program Facilitator",
+      date: "Feb 2024 - May 2024",
+      location: "Girls Who Code with Renaissance Academy at Fisher",
+      description: "",
+      tags: [],
+    },
+
+  ],
+};
