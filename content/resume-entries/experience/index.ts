fa6fba533a9d683ec7b9f5e61d43b5afe7a73bf2
drixitel <drixitel@gmail.com>
2024-07-27 22:33:10 -0700

    update experience

--- content/resume-entries/experience/index.ts
+++ content/resume-entries/experience/index.ts
@@ -4,19 +4,27 @@ import { PrettyLady } from "./pretty-lady";
 export const experience: ResumeSection = {
   title: "Experience",
   entries: [
+    // {
+    //   title: "Pretty Lady",
+    //   location: "Everywhere",
+    //   date: "Forever",
+    //   description: "I am a pretty lady.",
+    //   tags: ["Pretty", "Lady"],
+    //   RenderPage: PrettyLady,
+    // },
     {
-      title: "Freelance Software Engineer",
-      date: "2021 - Present",
-      description: "I am a software engineer.",
-      tags: ["React", "TypeScript", "Node.js", "GraphQL", "AWS"],
+      title: "Semiconductor Patent Examiner",
+      location: "US Patent & Trademark Office",
+      date: "2024 - Present",
+      description: "",
+      tags: [],
     },
     {
-      title: "Pretty Lady",
-      location: "Everywhere",
-      date: "Forever",
-      description: "I am a pretty lady.",
-      tags: ["Pretty", "Lady"],
-      RenderPage: PrettyLady,
+      title: "Junior Specialist",
+      location: "Santa Cruz Institute for Particle Physics",
+      date: "2023 - 2024",
+      description: "R&D Software Developer",
+      tags: [],
     },
   ],
 };
