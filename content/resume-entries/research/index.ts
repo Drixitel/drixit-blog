7391673cc3765a7aa6865f5128521bd6735b5a14
drixitel <drixitel@gmail.com>
2024-07-27 22:33:20 -0700

    added research entry

---
+++ content/resume-entries/research/index.ts
@@ -0,0 +1,24 @@
+import { ResumeSection } from "..";
+
+
+
+export const research: ResumeSection = {
+  title: "Research",
+  entries: [
+    {
+      title: "Remote Web Development Internship",
+      date: "Jun 2021 - Sep 2021",
+      location: "US Department of Energy",
+      description: "",
+      tags: [],
+    },
+    {
+      title: "Entry, Decent, & Landing Internship",
+      date: "Jan 2020 - Aug 2020",
+      location: "College of the Desert Internship funded by NASA/JPL",
+      description: "",
+      tags: [],
+    },
+
+  ],
+};
