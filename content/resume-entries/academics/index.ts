e5b8c9c029816c596d561c812f0b5ff49e9690c1
drixitel <drixitel@gmail.com>
2024-07-27 22:32:57 -0700

    update academics

--- content/resume-entries/academics/index.ts
+++ content/resume-entries/academics/index.ts
@@ -8,42 +8,34 @@ export const academics: ResumeSection = {
     {
       title: "Bachelor of Science in Applied Physics",
       location: "University of California, Santa Cruz",
-      date: "2021 - 2023",
+      date: "2023",
       description:
-        "I graduated with a Bachelor of Science in Applied Physics from the University of California, Santa Cruz.",
+        "",
       tags: ["Computer Science", "Bachelor of Science"],
     },
     {
       title: "Bachelor of Arts in Mathematics",
       location: "University of California, Santa Cruz",
-      date: "2021 - 2023",
+      date: "2023",
       description:
-        "I graduated with a Bachelor of Arts in Mathematics from the University of California, Santa Cruz.",
+        "",
       tags: ["Mathematics", "Bachelor of Arts"],
     },
-    {
-      title: "Associate of Science in Physics",
-      location: "College of the Desert",
-      date: "2019 - 2021",
-      description:
-        "I graduated with an Associate of Science in Physics from College of the Desert.",
-      tags: ["Physics", "Associate of Science"],
-    },
-    {
-      title: "Associate of Science in Mathematics",
-      location: "College of the Desert",
-      date: "2019 - 2021",
-      description:
-        "I graduated with an Associate of Science in Mathematics from College of the Desert.",
-      tags: ["Mathematics", "Associate of Arts"],
-    },
-    {
-      title: "Associate of Arts in Psychology",
-      location: "College of the Desert",
-      date: "2019 - 2021",
-      description:
-        "I graduated with an Associate of Arts in Psychology from College of the Desert.",
-      tags: ["Psychology", "Associate of Arts"],
-    },
+    // {
+    //   title: "Associate of Science in Physics",
+    //   location: "College of the Desert",
+    //   date: "2021",
+    //   description:
+    //     "",
+    //   tags: ["Physics", "Associate of Science"],
+    // },
+    // {
+    //   title: "Associate of Science in Mathematics",
+    //   location: "College of the Desert",
+    //   date: "2021",
+    //   description:
+    //     "",
+    //   tags: ["Mathematics", "Associate of Arts"],
+    // },
   ],
 };
