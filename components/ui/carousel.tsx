6bab0dcace9d127aa28017db60cedeed0818cef6
drixitel <drixitel@gmail.com>
2025-09-28 18:28:13 -0700

    adjusted carousel definition

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -60,6 +60,7 @@ const Carousel = React.forwardRef<
   ) => {
     const [carouselRef, api] = useEmblaCarousel(
       {
+        align: "start",
         ...opts,
         axis: orientation === "horizontal" ? "x" : "y",
       },
